fc363fa6b14ea44096170a3ef222081b6ccc0604
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-02-06 01:40:28 +0500

    Frontend added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'antd/dist/antd.css';
+import './App.css';
+import {Button} from "antd";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Button>Click me</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
