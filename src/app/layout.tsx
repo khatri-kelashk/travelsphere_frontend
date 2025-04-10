9202557ceb9107dd72b3795101dbd1e670b4344b
Kelash Kumar Khatri <khatri.kelash@yahoo.com>
2025-04-11 01:00:18 +0500

    Fix: FORM component installed + used

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import { Toaster } from '@/components/ui/toaster'
+import { Toaster } from "@/components/ui/toaster"; //Check here, keep it or remove it.
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] })
